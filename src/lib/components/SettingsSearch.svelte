d3b074daac5ef55075032594126b36d96c78f060
nayan <nayan8teen@gmail.com>
2026-02-05 19:57:12 -0500

    pretty pretty

--- src/lib/components/SettingsSearch.svelte
+++ src/lib/components/SettingsSearch.svelte
@@ -17,7 +17,9 @@
 	let settings = $derived(deviceId ? deviceState.deviceSettings[deviceId] : undefined);
 
 	let searchableSettings = $derived(
-		getAllSettings(settings, true, true, false).filter((s) => !s.hidden || MODEL_SETTINGS.includes(s.key))
+		getAllSettings(settings, true, true, false).filter(
+			(s) => !s.hidden || MODEL_SETTINGS.includes(s.key)
+		)
 	);
 	let deviceValues = $derived(deviceId ? deviceState.deviceValues[deviceId] : undefined);
 
